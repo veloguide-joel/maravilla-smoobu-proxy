efd1ed6ec9a0438034b73e1ba493440959cd2dfd
Joel Goralski <hola@theaccidentalretiree.mx>
2026-02-06 17:26:42 -0600

    Fix CORS headers so browser fetch succeeds

--- api/_cors.js
+++ api/_cors.js
@@ -4,15 +4,38 @@ const ALLOWED_ORIGINS = new Set([
 ]);
 
 export function applyCors(req, res) {
-  const origin = req.headers.origin;
-
-  if (origin && ALLOWED_ORIGINS.has(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
+  if (res.__corsWrapped) {
+    return;
   }
 
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+  res.__corsWrapped = true;
+
+  const origin = req.headers.origin;
+  const allowOrigin = origin && ALLOWED_ORIGINS.has(origin);
+
+  const applyHeaders = () => {
+    if (allowOrigin) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+      res.setHeader("Vary", "Origin");
+    }
+
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+  };
+
+  const originalJson = res.json?.bind(res);
+  if (originalJson) {
+    res.json = (body) => {
+      applyHeaders();
+      return originalJson(body);
+    };
+  }
+
+  const originalEnd = res.end.bind(res);
+  res.end = (...args) => {
+    applyHeaders();
+    return originalEnd(...args);
+  };
 }
 
 export function handleCorsPreflight(req, res) {
