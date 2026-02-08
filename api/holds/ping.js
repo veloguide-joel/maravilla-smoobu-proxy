3ae6fac0e8c256048207f696deb4b4b7e470b910
Joel Goralski <hola@theaccidentalretiree.mx>
2026-02-08 12:57:47 -0600

    Make holds ping minimal for debug

--- api/holds/ping.js
+++ api/holds/ping.js
@@ -1,10 +1,3 @@
-import { applyCors, handleCorsPreflight } from "../_cors";
-
 export default function handler(req, res) {
-  if (handleCorsPreflight(req, res)) {
-    return;
-  }
-
-  applyCors(req, res);
   res.status(200).json({ ok: true, route: "holds/ping" });
 }
