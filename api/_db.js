4437c7d74fd4274c608a6d8256fcec55f83768fd
Joel Goralski <hola@theaccidentalretiree.mx>
2026-02-08 12:40:13 -0600

    Add Postgres db helper

---
+++ api/_db.js
@@ -0,0 +1,25 @@
+import { Pool } from "pg";
+
+let pool;
+
+function getPool() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is required to query the database");
+  }
+
+  if (!pool) {
+    pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  }
+
+  return pool;
+}
+
+export async function query(text, params) {
+  try {
+    const db = getPool();
+    return await db.query(text, params);
+  } catch (err) {
+    console.error(err.message);
+    throw err;
+  }
+}
