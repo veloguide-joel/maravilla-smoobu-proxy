3cc6fcea6101c0589375c250c439fc47f617631c
Joel Goralski <hola@theaccidentalretiree.mx>
2026-02-08 13:02:10 -0600

    Convert cors helper to ESM

--- api/_cors.js
+++ api/_cors.js
@@ -1,47 +1,10 @@
-const ALLOWED_ORIGINS = new Set([
-  "http://127.0.0.1:5500",
-  "http://localhost:5500"
-]);
+export default function cors(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-export function applyCors(req, res) {
-  if (res.__corsWrapped) {
-    return;
-  }
-
-  res.__corsWrapped = true;
-
-  const origin = req.headers.origin;
-  const allowOrigin = origin && ALLOWED_ORIGINS.has(origin);
-
-  const applyHeaders = () => {
-    if (allowOrigin) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-      res.setHeader("Vary", "Origin");
-    }
-
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
-  };
-
-  const originalJson = res.json?.bind(res);
-  if (originalJson) {
-    res.json = (body) => {
-      applyHeaders();
-      return originalJson(body);
-    };
-  }
-
-  const originalEnd = res.end.bind(res);
-  res.end = (...args) => {
-    applyHeaders();
-    return originalEnd(...args);
-  };
-}
-
-export function handleCorsPreflight(req, res) {
   if (req.method === "OPTIONS") {
-    applyCors(req, res);
-    res.status(204).end();
+    res.status(200).end();
     return true;
   }
 
