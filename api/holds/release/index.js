0f08420e466c2b08a36e9af67b33eca29cac6a95
Joel Goralski <hola@theaccidentalretiree.mx>
2026-02-13 15:45:39 -0600

    Fix CORS allow methods for holds endpoints

--- api/holds/release/index.js
+++ api/holds/release/index.js
@@ -2,7 +2,7 @@ import { pool } from "../../_db.js";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
